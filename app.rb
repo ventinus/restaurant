a28690e8370c26957c888d9d0caab7b42878a714
Jonathan Eddy <jonathanceddy@gmail.com>
2015-02-10 10:04:24 -0500

    syled for days

--- app.rb
+++ app.rb
@@ -14,8 +14,6 @@ end
 enable :sessions
 set :app_password, "fresh"
 
-
-
 get '/' do
 	@name = session[:username] || "Not logged in"
 	
@@ -35,19 +33,16 @@ get '/foods/new' do
 end
 
 post '/foods' do
-#post new food item
 	food = Food.create(params[:food])
 	redirect to "/foods"
 end
 
 get '/foods/:id/edit' do |id|
-#edit food item
 	@food = Food.find(id)
 	erb :'food/edit'
 end
 
 patch '/foods/:id' do |id|
-#update that food item
 	Food.find(id).update(params[:food])
 	redirect to "/foods"
 end
@@ -57,47 +52,37 @@ delete '/foods/:id' do |id|
 	redirect to "/foods"
 end
 
-
-
 ###########party
 
 get '/parties' do
-#display parties. 2 tables with current (having not paid) and past (having paid) both ordered by most recent
-#each party links the party_id to the order_id to show the ordered. if they havent ordered, show "not ordered yet"
 	@parties = Party.all
 	erb :'party/index'
 end
 
 get '/parties/new' do
-#create new party item
 	@available = Party.open_tables
 	erb :'party/new'
 end
 
 get '/parties/:id' do |id|
-#display the specificities of each party with order items
 	@party = Party.find(id)
-	@orders = Order.find_by_party_id(id)
+	# @orders = Order.find_by_party_id(id)
 	erb :'party/show'
 end
 
 post '/parties' do
-#post new party item
 	Party.create(params[:party])
 	Receipt.create(party_id: Party.last.id)
 	redirect to "/parties"
 end
 
 get '/parties/:id/edit' do |id|
-#edit party item. with button to complete order that changes paid to true and creates a txt file with receipt
 	@party = Party.find(id)
-	@available = Party.open_tables
 	@foods = Food.all
 	erb :'party/edit'
 end
 
 patch '/parties/:id' do |id|
-#update that party item
 	Party.find(id).update(params[:party])
 	Order.create(params[:order].merge({ party_id: id}))
 	redirect to "/parties/#{id}"
@@ -121,36 +106,17 @@ end
 #############order
 
 get '/orders' do
-#display order items
-	# @parties = Party.all.where(paid: 'f')
 	@orders = Order.all
-
 	erb :'order/index'
 end
 
-# get '/orders/new' do
-# #create new order item
-# 	@foods = Food.all
-# 	@unavailable = Party.used_tables
-# 	erb :'order/new'
-# end
-
-# post '/orders' do
-# #post new order item
-# 	Order.create(params[:order])
-# 	redirect to "/orders"
-# end
-
-
 get '/orders/:id/edit' do |id|
-#edit order item.
 	@order = Order.find(id)
 	@foods = Food.all
 	erb :'order/edit'
 end
 
 patch '/orders/:id' do |id|
-#update that order item
 	Order.find(id).update(params[:order])
 	redirect to "orders"	
 end
@@ -160,16 +126,7 @@ delete '/orders/:id' do |id|
 	redirect to "/orders"
 end
 
-get '/console' do
-	Pry.start(binding)
-end
-
 ##############checkout
-
-get 'receipts' do
-	@receipts = Receipt.all
-	erb :'/checkout/receipt'
-end
 
 get '/checkout/:id' do |id|
 	@party = Party.find(id)
@@ -177,11 +134,9 @@ get '/checkout/:id' do |id|
 	erb :'/checkout/show'
 end
 
-
 ###########closed
 
 get '/closed' do 
-	# shows all closed
 	@receipts = Receipt.all
 	erb :'checkout/index'
 end
@@ -196,8 +151,16 @@ patch '/closed/:id' do |id|
 end
 
 get '/closed/:id' do |id|
-	# shows details of individuals closed
 	@receipt = Receipt.find_by_party_id(id)
 	erb :'checkout/closed'
 end
 
+get '/print_closed/:id' do |id|
+	@receipt = Receipt.find_by_party_id(id)
+	erb :'checkout/bare_receipt'
+end
+
+############
+get '/console' do
+	Pry.start(binding)
+end
